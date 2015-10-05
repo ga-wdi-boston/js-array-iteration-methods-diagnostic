657750f0475c3e6a86dfb9dde182da3fd4a8bfb0
Antony Donovan <antony.donovan@generalassemb.ly>
2015-10-04 20:26:06 -0400

    Create assessment

---
+++ README.md
@@ -0,0 +1,15 @@
+![General Assembly Logo](http://i.imgur.com/ke8USTq.png)
+
+# Assessment for JavaScript Array methods
+
+You have 30 minutes
+
+## Instructions
+
+Fork, clone, branch (response), and npm install.
+
+Follow the prompts in `lib/assessment.js` and complete each question.  You may use any resource, other than someone else in the classroom, to help you complete this assessment.
+
+Check your answers by running `grunt test`.  Check your code by running `grunt nag`. You can reformat your code with `grunt reformat`.
+
+When you are done, commit, push your changes, and submit a pull request.
---
+++ spec/assessment.spec.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var assessment = require('../lib/assessment.js');
+
+describe('reduce', function() {
+
+  var array = [-1, -2, -3, -4, -5];
+
+  describe('sum', function() {
+
+    it('returns the sum of the array', function() {
+      expect(array.reduce(assessment.sum)).toBe(-15);
+    });
+
+  });
+
+  describe('min', function() {
+
+    it('returns the minimum of the array elements', function() {
+      expect(array.reduce(assessment.min)).toBe(-5);
+    });
+
+  });
+
+});
+
+describe('enumerable', function() {
+
+  var array = [-1, -2, -3, -4, -5];
+
+  describe('any', function() {
+
+    it('returns false for greater than 0 predicate', function() {
+      expect(assessment.any(array, n => n > 0)).toBe(false);
+    });
+
+    it('returns true for less than 0 predicate', function() {
+      expect(assessment.any(array, n => n < 0)).toBe(true);
+    });
+
+  });
+
+  describe('select', function() {
+
+    var answer = [-1, -3, -5];
+
+    it('returns correct array for not even predicate', function() {
+      expect(assessment.select(array, n => n % 2)).toEqual(answer);
+    });
+
+  });
+
+  describe('find', function() {
+
+    it('returns right number for equal `-3` predicate', function() {
+      expect(assessment.find(array, n => n === -3)).toBe(-3);
+    });
+
+  });
+
+});
